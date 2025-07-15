44d2a550761b74a9baae957ba01c5678a5d9c81a
lluis <llluis@hotmail.com>
2025-07-15 20:22:12 +0200

    posar etiqueta al al clau primaria de la taula

--- Sprint 4/Nivell 3/SQL_insercio_FK_transaction_products.sql
+++ Sprint 4/Nivell 3/SQL_insercio_FK_transaction_products.sql
@@ -1,12 +1,20 @@
+/* clau modificar tipis de dades a INT*/
+ALTER TABLE transaction_products
+MODIFY COLUMN product_id INT;
+
+/* clau primaria PK*/
+ALTER TABLE transaction_products
+ADD CONSTRAINT pk_transaction_products
+	PRIMARY KEY (transaction_id,product_id);
+
+/* clau foranea FK*/
 ALTER TABLE transaction_products
 ADD CONSTRAINT fk_transaction
   FOREIGN KEY (transaction_id) REFERENCES transactions(id)
   ON DELETE CASCADE
   ON UPDATE CASCADE;
 
-ALTER TABLE transaction_products
-MODIFY COLUMN product_id INT;
-
+/* clau foranea FK*/
 ALTER TABLE transaction_products
 ADD CONSTRAINT fk_product
   FOREIGN KEY (product_id) REFERENCES products(id)
